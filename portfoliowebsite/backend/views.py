96f2f0a8ca9eefac16fec77af5d549e72fa262d2
Cindy Long <cindycs212@gmail.com>
2023-01-19 22:17:39 -0500

    rm unused files

--- portfoliowebsite/backend/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def frontendViews(request):
-    context = {}
-    return render(request, 'index.html', context)
--- portfoliowebsite/contact/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ContactCreateView
-
-urlpatterns = [
-    # by keeping '', the url to access contact form remains api/contact/
-    path('', ContactCreateView.as_view()),
-]
